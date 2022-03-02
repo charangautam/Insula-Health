8956636f44a4dead3550d6b3098842425bf2c9fd
Charan Gautam <gautamsaicharan@yahoo.com>
2022-03-02 14:01:06 -0700

    register route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -4,9 +4,28 @@ import bcrypt from 'bcrypt';
 
 const router = express.Router();
 
-router.post('/register', (req, res) => {
-    res.status(200).json('it works!')
+router.post('/register', async (req, res) => {
+    try {
+        const checkUsername = await User.findOne({ username: req.body.username })
+        if (checkUsername) {
+            res.status(409).json('user with this username already exists')
+            return
+        }
+
+        // hash password
+        req.body.password = bcrypt.hash(req.body.password, 10)
+
+        const newUser = new User(req.body)
+        const user = newUser.save()
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
+router.post('/login', (req, res) => {
+
+})
 
 export default router;
