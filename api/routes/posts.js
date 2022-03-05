6f1a2e3992e7785767a38cff65c82a893e064c08
Charan Gautam <gautamsaicharan@yahoo.com>
2022-03-05 13:44:00 -0700

    get all posts route

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -61,5 +61,15 @@ postsRouter.delete('/:postId', verifyToken, async (req, res) => {
     }
 })
 
+// get all posts
+postsRouter.get('/', verifyToken, async (req, res) => {
+    try {
+        const posts = await Post.find()
+        res.status(200).json(posts)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 
 export default postsRouter;
