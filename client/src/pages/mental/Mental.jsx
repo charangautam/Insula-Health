6b0ab2c8a9c1e07b0b9d552da219431db47d819f
Charan Gautam <gautamsaicharan@yahoo.com>
2022-05-12 17:42:07 -0600

    back again

--- client/src/pages/mental/Mental.jsx
+++ client/src/pages/mental/Mental.jsx
@@ -9,6 +9,10 @@ function Mental() {
         <div className="mental">
             <img src={mental} alt="mental health" />
             <h1>Mental Health</h1>
+            <div className="explained">
+                <h2>Explained</h2>
+                <p>Mental health includes our emotional, psychological, and social well-being. It affects how we think, feel, and act. It also helps determine how we handle stress, relate to others, and make choices. Mental health is important at every stage of life, from childhood and adolescence through adulthood.</p>
+            </div>
         </div>
     )
 }
